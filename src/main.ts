624a1ab20535e70300dc3b32871f62be85bf4cdd
fyoussef <fillipyuou@gmail.com>
2025-04-24 12:27:10 -0300

    adjust

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ class Student {
     this.name = name;
     this.id = this.nextStudent++;
   }
+
   notesStudent(note: number): void {
     if (note >= 0 && note <= 100) {
       this.notes.push(note);
@@ -25,28 +26,28 @@ class Student {
   }
 
   lettersNotes(): string {
-    const letter = this.media();
-    if (letter >= 80) return "A";
-    if (letter >= 60.0 && letter <= 79.9) return "B";
-    if (letter >= 40.0 && letter <= 59.9) return "C";
-    if (letter >= 20.0 && letter <= 39.9) return "D";
+    const media = this.media();
+    if (media >= 80) return "A";
+    if (media >= 60.0 && media <= 79.9) return "B";
+    if (media >= 40.0 && media <= 59.9) return "C";
+    if (media >= 20.0 && media <= 39.9) return "D";
     return "F";
   }
 
   sumaryStudent(): string {
     const media = this.media().toFixed(2);
-    const letters = this.lettersNotes();
-    return `ID: ${this.id}, Aluno: ${this.name}, Media: ${media}, Nota-Letra:${letters}`;
+    const letter = this.lettersNotes();
+    return `ID: ${this.id}, Aluno: ${this.name}, Media: ${media}, Nota-Letra:${letter}`;
   }
 }
 
 class GradeBook {
   private students: Student[] = [];
 
-  addStudent(name: string) {
+  addStudent(name: string): Student {
     const student = new Student(name);
     this.students.push(student);
-    return true;
+    return student;
   }
 
   removeStudent(id: number): boolean {
@@ -58,5 +59,98 @@ class GradeBook {
     return false;
   }
 
-  get;
+  addNoteToStudent(id: number, note: number): boolean {
+    const index = this.students.findIndex((student) => student.id === id);
+    if (index !== -1) {
+      const student = this.students[index];
+      student.notesStudent(note);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private unnaccent(str: string) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+
+  searchStudentByName(keyword: string) {
+    const student = this.students.find((student) =>
+      this.unnaccent(student.name.toLowerCase().trim()).includes(
+        this.unnaccent(keyword.toLowerCase().trim())
+      )
+    );
+    if (!student) {
+      return undefined;
+    }
+    return student;
+  }
+
+  getStudentsMedia() {
+    const studentsMedia = this.students.reduce(
+      (acc, student) => (acc += student.media()),
+      0
+    );
+    const totalStudents = this.students.length;
+    return (studentsMedia / totalStudents).toFixed(2);
+  }
+
+  getStudentsLetterNotes() {
+    const studentsLetterNotes = this.students.reduce(
+      (acc, student) => {
+        acc[student.lettersNotes()] = acc[student.lettersNotes()] + 1;
+        return acc;
+      },
+      {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        F: 0,
+      }
+    );
+
+    return studentsLetterNotes;
+  }
+
+  getBestStudentNotes(limit: number) {
+    const ordered = this.students.sort((currentStudent, nextStudent) => {
+      return currentStudent.media() - nextStudent.media();
+    });
+    return ordered.slice(0, limit);
+  }
+
+  summaryGradeBook() {
+    const totalStudents = this.students.length;
+    const studentsMedia = this.getStudentsMedia();
+    const studentsLetterNotes = this.getStudentsLetterNotes();
+    return `Total de alunos: ${totalStudents}.\nMédia geral: ${studentsMedia}.\nMediana: NÃO FEITO.\nDistribuição de letras:\n${Object.entries(
+      studentsLetterNotes
+    )
+      .map(([letter, note]) => `${letter}: ${note}`)
+      .join("\n")}`;
+  }
 }
+
+const gradeBook = new GradeBook();
+const student1 = gradeBook.addStudent("Ana");
+gradeBook.addNoteToStudent(student1.id, 50);
+gradeBook.addNoteToStudent(student1.id, 60);
+
+const student2 = gradeBook.addStudent("Bruno");
+gradeBook.addNoteToStudent(student2.id, 50);
+gradeBook.addNoteToStudent(student2.id, 60);
+
+const student3 = gradeBook.addStudent("Carla");
+gradeBook.addNoteToStudent(student3.id, 60);
+gradeBook.addNoteToStudent(student3.id, 60);
+
+const student4 = gradeBook.addStudent("Diego");
+gradeBook.addNoteToStudent(student4.id, 60);
+gradeBook.addNoteToStudent(student4.id, 60);
+
+const student5 = gradeBook.addStudent("Eva");
+gradeBook.addNoteToStudent(student5.id, 60);
+gradeBook.addNoteToStudent(student5.id, 60);
+
+console.log(gradeBook.summaryGradeBook());
